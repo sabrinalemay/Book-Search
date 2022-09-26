016b0032a661b84f733ed50206b179714776ca10
Sabrina LeMay <b33na1997@gmail.com>
2022-09-26 15:29:41 -0700

    changed the spelling of gql in queries and added mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,4 @@
-import qgl from "graphql-tag";
+import gql from "graphql-tag";
 
 export const LOGIN_USER = gql`
     mutation login($email: String!, $password: String!){
@@ -22,7 +22,7 @@ export const ADD_USER = gql`
         }
     }`;
 
-export const SAVE_BOOK = qgl`
+export const SAVE_BOOK = gql`
     mutation saveBook($book: SavedBookInput!){
         saveBook(book: $book){
             username
